3865832a2acb32cbcbbaec1cf5600be13beaed71
Neugodnikova <Neugodnikova.ae@phystech.edu>
2023-01-06 17:16:34 +0500

    file

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+
+class YaUploader:
+
+    def __init__(self, token: str):
+        self.token = token
+        self.HOST = 'https://cloud-api.yandex.net/'
+
+    def get_headers(self):
+        return {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'}
+
+    def upload(self, path_to_file):
+        uri = 'v1/disk/resources/upload'
+        url = self.HOST + uri
+        file_name = path_to_file.split("\\")[-1]
+        params = {'path': file_name, 'overwrite': 'true'}
+        response = requests.get(url, headers=self.get_headers(), params=params)
+        upload_link = response.json()['href']
+        print(upload_link)
+        resp = requests.put(upload_link, headers=self.get_headers(), data=open(path_to_file, 'rb'))
+        if resp.status_code == 201 or 203:
+            print('Success')
+        else:
+            (print(f' Код ошибки {resp.status_code}'))
+
+
+if __name__ == '__main__':
+    token =
+    uploader = YaUploader(token)
+    result = uploader.upload(r'C:\Users\harle\PycharmProjects\8_2_api\file.txt')
